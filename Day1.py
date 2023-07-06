a2736c586178cf356c53602beca35cb678368494
SanskrutiJagtap3 <138582250+SanskrutiJagtap3@users.noreply.github.com>
2023-07-06 20:50:17 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,21 @@
+# print("Sanskruti", end=" ")
+# print("Jagtap")
+
+# a=20
+# print(type(a))
+
+
+# a="20"
+# print(int(a))
+
+# print()
+# n=int(input("Enter a num :")); print(type(n))
+
+n = input("Enter your name: ")
+print("The name of the student is:",n)
+m1 = int(input("Enter your marks:"))
+m2 = int(input())
+m3 = int(input())
+print ("marks:",m1,m2,m3)
+avg = (m1+m2+m3)/3
+print("Average=",avg)
